7c4c2e6e145f4cef1d13d1ce01c78ce1c626cd00
RajKKapadia <raajforyou@gmail.com>
2024-12-29 10:30:27 +0530

    updated packages

--- gradio_ui.py
+++ gradio_ui.py
@@ -6,7 +6,7 @@ with gr.Blocks(
     css='footer {visibility: hidden}'
 ) as demo:
     chatbot = gr.Chatbot(label='Talk to SQL ChatBot',
-                         bubble_full_width=False)
+                         bubble_full_width=False,)
     msg = gr.Textbox(label='Query', placeholder='Enter text and press enter')
     clear = gr.ClearButton([msg, chatbot], variant='stop')
     msg.submit(
